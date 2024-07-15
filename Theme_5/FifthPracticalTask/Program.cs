87b470c57d13db315d28c2fc5bff75c62423053c
Sangvanaht <slippingtester@gmail.com>
2024-07-15 22:02:44 +0300

    Fifth Practical Task complite!
    The solution to the practical task of Theme_5 has been added.

---
+++ Theme_5/FifthPracticalTask/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FifthPracticalTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите текст: ");
+            string text = Console.ReadLine();
+            string[] split = SplitText(text);
+            PrintStringArray(split);
+        }
+
+        /// <summary>
+        /// Разделяет text на массив string, разделителем является пробельный символ.
+        /// </summary>
+        /// <param name="text"> Разделяемая строка</param>
+        /// <returns> Возвращает полученный в результате деления массив строк</returns>
+        static string[] SplitText(string text)
+        {
+            int wordCount = 0;
+            foreach (char c in text)
+            {
+                if (c == ' ') wordCount++;
+            }
+            wordCount = wordCount == text.Length ? 0 : wordCount + 1;
+
+            string[] result = new string[wordCount];
+
+            for (int i = 0, j = 0 ; i < text.Length && j != wordCount; i++, j++)
+            {
+                string temp = "";
+                while (i < text.Length && text[i] != ' ')
+                {
+                    temp += text[i];
+                    i++;
+                }
+                result[j] = temp;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Выводит на экран консоли элементы массива,каждый с новой строки
+        /// </summary>
+        /// <param name="arr">Массив строк выводимый в консоль</param>
+        static void PrintStringArray(string[] arr)
+        {
+            if (arr.Length == 0)
+            {
+                Console.WriteLine("Вы ничего не ввели!");
+                return;
+            }
+            foreach (string str in arr)
+            {
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
---
+++ Theme_5/FifthPracticalTaskP2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FifthPracticalTaskP2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите текст: ");
+            string text = Console.ReadLine();
+            Console.WriteLine(ReversWords(text));
+        }
+
+        /// <summary>
+        /// Разделяет text на массив string, разделителем является пробельный символ.
+        /// </summary>
+        /// <param name="text"> Разделяемая строка</param>
+        /// <returns> Возвращает полученный в результате деления массив строк</returns>
+        static string[] SplitText(string text)
+        {
+            int wordCount = 0;
+            foreach (char c in text)
+            {
+                if (c == ' ') wordCount++;
+            }
+            wordCount = wordCount == text.Length ? 0 : wordCount + 1;
+
+            string[] result = new string[wordCount];
+
+            for (int i = 0, j = 0; i < text.Length && j != wordCount; i++, j++)
+            {
+                string temp = "";
+                while (i < text.Length && text[i] != ' ')
+                {
+                    temp += text[i];
+                    i++;
+                }
+                result[j] = temp;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Переставляет слова в строке в обратном порядке
+        /// </summary>
+        /// <param name="inputPhrase"> Входная строка</param>
+        /// <returns>Измененная строка</returns>
+        static string ReversWords(string inputPhrase)
+        {
+            string[] arr = SplitText(inputPhrase);
+            string result = "";
+            for (int i = 1; i <= arr.Length; i++)
+            {
+                result += arr[arr.Length - i] + " ";
+            }
+
+            return result;
+        }
+    }
+}
